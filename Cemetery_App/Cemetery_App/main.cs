381679e84bb2d7b735ae4f11305d9494a5b41d94
Andrés Obando Alfaro <31627028+Andres49815@users.noreply.github.com>
2018-07-05 12:35:52 -0600

    Cambio Hola Mundo
    Hola mundo Lul

---
+++ Cemetery_App/Cemetery_App/main.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cemetery_App
+{
+    class main
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hola Mundo");
+            Console.WriteLine("LUL");
+            Console.ReadKey();
+        }
+    }
+}
